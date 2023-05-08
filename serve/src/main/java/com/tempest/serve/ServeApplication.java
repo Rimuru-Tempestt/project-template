f0f14cd79d2c84340410897986f786b1d0b0e0c0
Rimuru-Tempestt <1916514113@qq.com>
2023-05-08 13:02:17 +0800

    初始化

---
+++ serve/src/main/java/com/tempest/serve/ServeApplication.java
@@ -0,0 +1,16 @@
+package com.tempest.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Rimuru Tempest
+ */
+@SpringBootApplication
+public class ServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeApplication.class, args);
+    }
+
+}
